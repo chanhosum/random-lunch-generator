9059b0f66914cb8b2c146586ef2c5ba8335c2d97
chanhosum <chanhosum1@gmail.com>
2019-01-07 22:30:11 +0800

    add people

--- js/setting.js
+++ js/setting.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    function lowFirstLetter(string) {
+        return string.charAt(0).toLowerCase() + string.slice(1);
+    }
     function getFullList(){
         $.ajax({
             type: "GET",
@@ -8,27 +14,61 @@ $(document).ready(function(){
                 fullListLength = data.length;
                 for(var i in data) {
                     $(".restaurant").append("<label for='checkbox-"+i+"'>"+data[i].name+"</label>\n"+
-                        "        <input type='checkbox' name='checkbox-"+i+"' id='checkbox-"+i+"'>");
+                        "        <input type='checkbox' class='in1' name='checkbox-"+i+"' id='checkbox-"+i+"'>");
                 }
-                $( "input" ).checkboxradio();
+                $( ".in1" ).checkboxradio();
+            }
+        });
+    }
+    function getMemberList(){
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:8099/getMemberList",
+            dataType: "json",
+            success: function(data) {
+                fullListLength = data.length;
+                for(var i in data) {
+                    $(".member").append("<label for='mcheckbox-"+i+"'>"+capitalizeFirstLetter(data[i].name)+"</label>\n"+
+                        "        <input type='checkbox' class='in2' name='mcheckbox-"+i+"' id='mcheckbox-"+i+"'>");
+                }
+                $( ".in2" ).checkboxradio();
             }
         });
     }
     getFullList();
+    getMemberList();
     $( "#delete" ).on( "click", function( event) {
         var r = confirm("你是否要刪除嗎？");
         if (r == true) {
             var deleteArray = [];
-            $('.ui-checkboxradio-checked').each(function(){
+            $('.restaurant .ui-checkboxradio-checked').each(function(){
                 deleteArray.push($(this)[0].innerText.trim());
             });
             $.ajax({
                 type: "POST",
-                url: "https://random-lunch-list.herokuapp.com/delete",
+                url: "http://localhost:8099/delete",
                 contentType: 'application/json',
                 data: JSON.stringify(deleteArray),
                 success: function(data) {
                    location.reload();
+                }
+            });
+        }
+    });
+    $( "#deleteMember" ).on( "click", function( event) {
+        var r = confirm("你是否要刪除嗎？");
+        if (r == true) {
+            var deleteArray = [];
+            $('.member .ui-checkboxradio-checked').each(function(){
+                deleteArray.push(lowFirstLetter($(this)[0].innerText.trim()));
+            });
+            $.ajax({
+                type: "POST",
+                url: "http://localhost:8099/deleteMember",
+                contentType: 'application/json',
+                data: JSON.stringify(deleteArray),
+                success: function(data) {
+                    location.reload();
                 }
             });
         }
@@ -40,6 +80,18 @@ $(document).ready(function(){
             url: "https://random-lunch-list.herokuapp.com/add",
             contentType: 'application/json',
             data: JSON.stringify({text:addText}),
+            success: function(data) {
+                location.reload();
+            }
+        });
+    });
+    $( "#addMember" ).on( "click", function( event) {
+        var addText = $("#addMemberText").val();
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8099/addMember",
+            contentType: 'application/json',
+            data: JSON.stringify({text:lowFirstLetter(addText)}),
             success: function(data) {
                 location.reload();
             }
